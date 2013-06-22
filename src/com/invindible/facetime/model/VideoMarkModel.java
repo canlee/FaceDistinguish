8cc272befac158962b293f6664660762acc853db
Administrator <Administrator@2012-20110606UN>
2013-06-23 02:19:38 +0800

    视频小波

--- src/com/invindible/facetime/model/VideoMarkModel.java
+++ src/com/invindible/facetime/model/VideoMarkModel.java
@@ -1,8 +1,8 @@
 package com.invindible.facetime.model;
 
 public class VideoMarkModel {
-	private int mark;
-	private double[] dis;
+	private int mark; //标记识别为第几个人
+	private double[] dis; //标记4个距离 分别是l2 l1 马氏 最小距离
 	public int getMark() {
 		return mark;
 	}
