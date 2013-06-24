d144410eee147b2d15daaf3cdf2d1a21e6b02298
Administrator <Administrator@2012-20110606UN>
2013-06-24 17:55:10 +0800

    识别

--- src/com/invindible/facetime/algorithm/Mark.java
+++ src/com/invindible/facetime/algorithm/Mark.java
@@ -16,8 +16,8 @@ public class Mark {
 	}
 	
 	public static int[] mark(double[][] test,double[][] model,double[] testMean,double[][] modelMean,double[] allMean){
-		double value=L2Form.value(modelMean,allMean);		//l2 domain 
-		double l1value=L1Form.l1value(modelMean,allMean);   //l1 domain			
+		double value=L2Form.value(model,allMean);		//l2 domain 
+		double l1value=L1Form.l1value(model,allMean);   //l1 domain			
 		double[] madis=MixedMahalnobisDistance.calMixedMahalnobisDistance(testMean,modelMean);	 //ma distance	
 		double[] mindis=MixedMahalnobisDistance.calMinDistance(testMean, modelMean);  //min distance
 		int matmp=Identify.mark(Double.MAX_VALUE,madis);	//ma domain
@@ -26,32 +26,35 @@ public class Mark {
 		double[] facedis;  //l2 distance
 		double[] l1dis;   //l1 distance
 		int[] record=new int[3];
+		
+		System.out.println("l1+l2 value:"+value+" "+l1value);
+		
 		for(int i=0;i<=test.length;i++){
 			boolean l2,l1;
 			int[] identify=new int [6];
 			int error1=0,error2=0; //error2--l2  error1--l1
 			if(i==test.length){
-				l2=L2Form.inL2(testMean, allMean, value);
-				l1=L1Form.inL1(testMean, allMean, l1value);	//whether in or not
-				if(!l1||!l2)
-				{
-					record[i]=-1;
-					System.out.println("不在范围内");
-					continue;
-				}
+//				l2=L2Form.inL2(testMean, allMean, value);
+//				l1=L1Form.inL1(testMean, allMean, l1value);	//whether in or not
+//				if(!l1||!l2)
+//				{
+//					record[i]=-1;
+//					System.out.println("不在范围内");
+//					continue;
+//				}
 				facedis=L2Form.L2Form(model, testMean);  //calculate l2 distance
 				l1dis=L1Form.L1Form(model, testMean);   //calculate l1 distance
 			}
 			else
 			{
-				l2=L2Form.inL2(test[i], allMean, value);
-				l1=L1Form.inL1(test[i], allMean, l1value);
-				if(!l1||!l2)
-				{
-					record[i]=-1;
-					System.out.println("不在范围内");
-					continue;
-				}
+//				l2=L2Form.inL2(test[i], allMean, value);
+//				l1=L1Form.inL1(test[i], allMean, l1value);
+//				if(!l1||!l2)
+//				{
+//					record[i]=-1;
+//					System.out.println("不在范围内");
+//					continue;
+//				}
 				facedis=L2Form.L2Form(model, test[i]);  //calculate l2 distance
 				l1dis=L1Form.L1Form(model, test[i]);   //calculate l1 distance
 			}			
